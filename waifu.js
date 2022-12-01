b583b78d1aec1dffc9a8bd4896bd618be3ddfd8c
l0st-idi0t <carlnod121@gmail.com>
2022-12-01 15:52:49 -0600

    working

--- waifu.js
+++ waifu.js
@@ -1,21 +1,15 @@
 //fetch from to https://api.waifu.pics/
-
 let imgElem = document.getElementById("waifu-img");
 
-async function getWaifu() {
-    const response = await fetch("https://api.waifu.pics/sfw/waifu");
-    const data = await response.json();
-    return data;
-}
-
-
-let button = document.getElementById("waifu");
-if (button.addEventListener)
-    button.addEventListener("click", clickedButton, false);
-
 function clickedButton() {
-    getWaifu().then(data => {
+    getWaifu('sfw', 'waifu').then(data => {
         console.log(data['url']);
         imgElem.src = data['url'];
     });
+}
+
+async function getWaifu(type, category) {
+    const response = await fetch(`https://api.waifu.pics/${type}/${category}`);
+    const data = await response.json();
+    return data;
 }
